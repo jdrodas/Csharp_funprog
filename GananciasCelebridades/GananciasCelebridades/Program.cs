7055e4b6dca5c0333814ff5302a1ffbf2a1ae6a5
jdrodas <jdrodas@hotmail.com>
2022-03-06 17:22:41 -0500

    Agregada función IdentificaBeneficiado

--- GananciasCelebridades/GananciasCelebridades/Program.cs
+++ GananciasCelebridades/GananciasCelebridades/Program.cs
@@ -5,21 +5,21 @@ Contacto:       Juan Dario Rodas - jdrodas@hotmail.com
 Propósito:
 ----------
 Realice un programa en C# que calcule las ganancias de 4 celebridades, a partir de sus salarios. 
-Estas 4 celebridades son:
 
+Estas 4 celebridades son:
 - Gabriel: Su pago mensual es de 4,000$.
 - Ana: Su pago mensual es de 3,000$.
 - Catalina: Su pago mensual es de 4,500$.
 - Francisco: Su pago mensual es de 5,800$
 
 En el país en el que viven, hay una ley de impuestos que estipula que:
-
 - A partir de 1,000$, se les descuenta un 5%.
 - A partir de 3,500$, se les descuenta un 5% adicional.
 - A partir de 4,500$, se les descuenta un 10% adicional.
-- Si la cifra es superior a 5,500$, se les descuenta un 35% único. Esto quiere decir, que no se adicionan los impuestos anteriores.
+- Si la cifra es superior a 5,500$, se les descuenta un 35% único. 
+  Esto quiere decir, que no se adicionan los impuestos anteriores.
 
-*/
+ */
 
 using System;
 
@@ -37,7 +37,7 @@ namespace GananciasCelebridades
 
 
             for (int i = 0; i < nombreCelebridades.Length; i++)
-                Console.WriteLine($"{nombreCelebridades[i]}, Salario: {salariosCelebridades[i]}");
+                Console.WriteLine($"{nombreCelebridades[i]}, Sueldo: {salariosCelebridades[i]}");
 
             //Identificamos el tasa de impuestos aplicados a cada celebridad
             float[] tasaImpuestos = ObtieneTasaImpuestos(salariosCelebridades);
@@ -55,7 +55,13 @@ namespace GananciasCelebridades
                 Console.WriteLine($"{nombreCelebridades[i]}, Tasa impuestos: {(tasaImpuestos[i] * 100)}%, " +
                     $"Descuento por impuestos: {descuentosImpuestos[i]}");
 
-            Console.WriteLine("\n\n");
+            //Aqui calculamos el salario neto después de impuestos
+            float[] salariosNetos = new float[salariosCelebridades.Length];
+
+            for (int i = 0; i < salariosCelebridades.Length; i++)
+                salariosNetos[i] = salariosCelebridades[i] - descuentosImpuestos[i];
+
+            IdentificaBeneficiado(nombreCelebridades, salariosNetos);
 
         }
 
@@ -88,6 +94,35 @@ namespace GananciasCelebridades
             }
 
             return resultadoTasas;
+        }
+
+        /// <summary>
+        /// Identifica quien es la celebridad más beneficiada después de impuestos
+        /// </summary>
+        /// <param name="arregloNombres">Arreglo con los nombres de las celebridades</param>
+        /// <param name="arregloSueldos">Arreglo con los sueldos netos</param>
+        static void IdentificaBeneficiado(string[] arregloNombres, float[] arregloSueldos)
+        {
+            int posicionBeneficiado = 0;
+            float salarioMayor = arregloSueldos[posicionBeneficiado];
+
+            for (int i = 0; i < arregloSueldos.Length; i++)
+            {
+                if (arregloSueldos[i] > salarioMayor)
+                {
+                    salarioMayor = arregloSueldos[i];
+                    posicionBeneficiado = i;
+                }
+            }
+
+            Console.WriteLine($"\n\nSueldo netos de las celebridades:");
+
+            for (int i = 0; i < arregloNombres.Length; i++)
+                Console.WriteLine($"{arregloNombres[i]}, Sueldo neto: {arregloSueldos[i]}");
+
+            //Aqui escribimos el resultado:
+            Console.WriteLine($"\nLa celebridad con mayores ingresos después " +
+                $"de impuestos es: {arregloNombres[posicionBeneficiado]}");
         }
     }
 }
