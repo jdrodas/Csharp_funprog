442af11d8c6affe247595421bd12791308610b1d
jdrodas <jdrodas@hotmail.com>
2022-05-03 17:09:29 -0500

    Agregado proyecto SimRadioOO

---
+++ SimRadioOO/SimRadioOO/Form1.cs
@@ -0,0 +1,131 @@
+﻿/*
+Programa:       SimRadioOO
+Contacto:       Juan Dario Rodas - jdrodas@hotmail.com
+
+Enunciado al problema:
+----------------------
+
+Realice un programa en C# con interfaz gráfica en Windows Forms que simule 
+el comportamiento de un radio FM.
+
+El programa permitirá cambiar la frecuencia de la emisora a través de dos botones
+(F+,F-) que realizarán incrementos de la frecuencia de a 1 MHz. Las frecuencias están
+en el rango comprendido entre 88.9 y 107.9 MHz. El valor predeterminado para la frecuencia
+actual cuando comienza el programa será de 88.9 MHz. Si se incrementa el valor 
+por encima del límite superior, el valor siguiente será el de la frecuencia del límite 
+inferior. De manera equivalente, si se decrementa por debajo del límite inferior, el valor
+siguiente será el del límite superior. Con F+ y F- se podrá dar ciclos completos dentro
+del rango de frecuencias especificado.
+
+El programa permitirá cambiar el valor del volumen a través de dos botones (V+,V-), los cuales
+realizarán incrementos de 1 unidad en un rango de valores entre 0 y 10. Si se llega a los
+límites inferiores o superiores, los valores no podrán superar o decrementar dichos límites.
+El valor predeterminado del volumen será 0.
+
+El programa tendrá un botón de encendido y apagado, el cual no permitirá que las funciones
+se ejecuten en caso de que el radio esté apagado. Cuando el radio está apagado, los valores
+para la frecuencia y el volumen no se ven y los botones no funcionan 
+
+
+Nota Importante:
+----------------
+
+Esta versión del proyecto tiene separada la funcionalidad de presentación y lógica en clases
+independientes, Form1 y Radio. Es una versión mejorada del proyecto inicial que se encuentra aqui:
+
+https://github.com/jdrodas/Csharp_funprog/tree/master/SimRadio
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimRadioOO
+{
+    public partial class Form1 : Form
+    {
+        //Atributo de la clase
+        private Radio miRadio;
+
+        public Form1()
+        {
+            InitializeComponent();
+            miRadio = new Radio();
+        }
+
+        private void botonEncendido_Click(object sender, EventArgs e)
+        {
+            //Cambiamos el estado de encendido del radio
+            miRadio.CambiaEncendido();
+
+            //Aqui cambiamos el fondo del botón dependiendo del estado
+            ValidaEstadoRadio();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ValidaEstadoRadio();
+        }
+
+        private void ValidaEstadoRadio()
+        {
+            //Aqui cambiamos el fondo del botón dependiendo del estado
+            if (miRadio.Encendido == true)
+            {
+                botonEncendido.BackColor = Color.Green;
+                etiquetaValorFrecuencia.Text = miRadio.Frecuencia.ToString();
+                etiquetaValorVolumen.Text = miRadio.Volumen.ToString();
+
+                botonSubeVolumen.Enabled = true;
+                botonBajaVolumen.Enabled = true;
+
+                botonSubeFrecuencia.Enabled = true;
+                botonBajaFrecuencia.Enabled = true;
+            }
+            else
+            {
+                botonEncendido.BackColor = Color.Red;
+                etiquetaValorFrecuencia.Text = "";
+                etiquetaValorVolumen.Text = "";
+
+                botonSubeVolumen.Enabled = false;
+                botonBajaVolumen.Enabled = false;
+
+                botonSubeFrecuencia.Enabled = false;
+                botonBajaFrecuencia.Enabled = false;
+            }
+        }
+
+        private void botonSubeVolumen_Click(object sender, EventArgs e)
+        {
+            miRadio.SubirVolumen();
+            etiquetaValorVolumen.Text = miRadio.Volumen.ToString();
+        }
+
+        private void botonBajaVolumen_Click(object sender, EventArgs e)
+        {
+            miRadio.BajarVolumen();
+            etiquetaValorVolumen.Text = miRadio.Volumen.ToString();
+
+        }
+
+        private void botonSubeFrecuencia_Click(object sender, EventArgs e)
+        {
+            miRadio.SubirFrecuencia();
+            etiquetaValorFrecuencia.Text = miRadio.Frecuencia.ToString();
+        }
+
+        private void botonBajaFrecuencia_Click(object sender, EventArgs e)
+        {
+            miRadio.BajarFrecuencia();
+            etiquetaValorFrecuencia.Text = miRadio.Frecuencia.ToString();
+        }
+    }
+}
---
+++ SimRadioOO/SimRadioOO/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("SimRadioOO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimRadioOO")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("25385e05-67f4-47ad-8ecf-4835bd1507f6")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
