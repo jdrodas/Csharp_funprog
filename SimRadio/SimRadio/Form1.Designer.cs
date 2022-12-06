7578ae58865cb2ae933df9c724afdf88bd02d40d
Juan Dario Rodas <jdrodas@hotmail.com>
2022-12-05 20:52:13 -0500

    Proyecto actualizado a .NET 7

--- SimRadio/SimRadio/Form1.Designer.cs
+++ SimRadio/SimRadio/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        /// Variable del diseñador necesaria.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpiar los recursos que se estén usando.
+        ///  Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,14 +20,15 @@
             base.Dispose(disposing);
         }
 
-        #region Código generado por el Diseñador de Windows Forms
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.botonEncendido = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.etiquetaValorFrecuencia = new System.Windows.Forms.Label();
@@ -154,7 +155,6 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
@@ -170,4 +170,3 @@
         private System.Windows.Forms.Button botonBajaFrecuencia;
     }
 }
-
--- SimRadio/SimRadio/Form1.cs
+++ SimRadio/SimRadio/Form1.cs
@@ -1,37 +1,37 @@
-Ôªø/*
+/*
 Programa:       SimRadio
 Contacto:       Juan Dario Rodas - jdrodas@hotmail.com
 
 Enunciado al problema:
 ----------------------
 
-Realice un programa en C# con interfaz gr√°fica en Windows Forms que simule 
+Realice un programa en C# con interfaz gr·fica en Windows Forms que simule 
 el comportamiento de un radio FM.
 
-El programa permitir√° cambiar la frecuencia de la emisora a trav√©s de dos botones
-(F+,F-) que realizar√°n incrementos de la frecuencia de a 1 MHz. Las frecuencias est√°n
+El programa permitir· cambiar la frecuencia de la emisora a travÈs de dos botones
+(F+,F-) que realizar·n incrementos de la frecuencia de a 1 MHz. Las frecuencias est·n
 en el rango comprendido entre 88.9 y 107.9 MHz. El valor predeterminado para la frecuencia
-actual cuando comienza el programa ser√° de 88.9 MHz. Si se incrementa el valor 
-por encima del l√≠mite superior, el valor siguiente ser√° el de la frecuencia del l√≠mite 
-inferior. De manera equivalente, si se decrementa por debajo del l√≠mite inferior, el valor
-siguiente ser√° el del l√≠mite superior. Con F+ y F- se podr√° dar ciclos completos dentro
+actual cuando comienza el programa ser· de 88.9 MHz. Si se incrementa el valor 
+por encima del lÌmite superior, el valor siguiente ser· el de la frecuencia del lÌmite 
+inferior. De manera equivalente, si se decrementa por debajo del lÌmite inferior, el valor
+siguiente ser· el del lÌmite superior. Con F+ y F- se podr· dar ciclos completos dentro
 del rango de frecuencias especificado.
 
-El programa permitir√° cambiar el valor del volumen a trav√©s de dos botones (V+,V-), los cuales
-realizar√°n incrementos de 1 unidad en un rango de valores entre 0 y 10. Si se llega a los
-l√≠mites inferiores o superiores, los valores no podr√°n superar o decrementar dichos l√≠mites.
-El valor predeterminado del volumen ser√° 0.
+El programa permitir· cambiar el valor del volumen a travÈs de dos botones (V+,V-), los cuales
+realizar·n incrementos de 1 unidad en un rango de valores entre 0 y 10. Si se llega a los
+lÌmites inferiores o superiores, los valores no podr·n superar o decrementar dichos lÌmites.
+El valor predeterminado del volumen ser· 0.
 
-El programa tendr√° un bot√≥n de encendido y apagado, el cual no permitir√° que las funciones
-se ejecuten en caso de que el radio est√© apagado. Cuando el radio est√° apagado, los valores
+El programa tendr· un botÛn de encendido y apagado, el cual no permitir· que las funciones
+se ejecuten en caso de que el radio estÈ apagado. Cuando el radio est· apagado, los valores
 para la frecuencia y el volumen no se ven y los botones no funcionan 
 
 Nota Importante:
 ----------------
 
-Esta versi√≥n del proyecto tiene mezclada la funcionalidad de presentaci√≥n y l√≥gica en una
-sola clase, Form1. En una versi√≥n mejorada, deber√≠a existir una clase "Logica" con el 
-comportamiento del radio y la forma solo quede con los elementos de visualizaci√≥n.
+Esta versiÛn del proyecto tiene mezclada la funcionalidad de presentaciÛn y lÛgica en una
+sola clase, Form1. En una versiÛn mejorada, deberÌa existir una clase "Logica" con el 
+comportamiento del radio y la forma solo quede con los elementos de visualizaciÛn.
 
 */
 
@@ -41,11 +41,10 @@ using System.Drawing;
 using System.Windows.Forms;
 
 namespace SimRadio
-{ 
+{
     public partial class Form1 : Form
     {
         //Atributo de la clase
-        //private Radio miRadio;
         private float frecuencia;
         private int volumen;
         private bool encendido;
@@ -58,74 +57,73 @@ namespace SimRadio
             frecuencia = 88.9f;
             encendido = false;
         }
-
-    private void botonEncendido_Click(object sender, EventArgs e)
-    {
-        //Cambiamos el estado de encendido del radio
-        CambiaEncendido();
-
-        //Aqui cambiamos el fondo del bot√≥n dependiendo del estado
-        ValidaEstadoRadio();
-    }
-
-    private void Form1_Load(object sender, EventArgs e)
-    {
-        ValidaEstadoRadio();
-    }
-
-    private void ValidaEstadoRadio()
-    {
-        //Aqui cambiamos el fondo del bot√≥n dependiendo del estado
-        if (encendido == true)
+        private void botonEncendido_Click(object sender, EventArgs e)
         {
-            botonEncendido.BackColor = Color.Green;
-            etiquetaValorFrecuencia.Text = frecuencia.ToString();
+            //Cambiamos el estado de encendido del radio
+            CambiaEncendido();
+
+            //Aqui cambiamos el fondo del botÛn dependiendo del estado
+            ValidaEstadoRadio();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ValidaEstadoRadio();
+        }
+
+        private void ValidaEstadoRadio()
+        {
+            //Aqui cambiamos el fondo del botÛn dependiendo del estado
+            if (encendido == true)
+            {
+                botonEncendido.BackColor = Color.Green;
+                etiquetaValorFrecuencia.Text = frecuencia.ToString();
+                etiquetaValorVolumen.Text = volumen.ToString();
+
+                botonSubeVolumen.Enabled = true;
+                botonBajaVolumen.Enabled = true;
+
+                botonSubeFrecuencia.Enabled = true;
+                botonBajaFrecuencia.Enabled = true;
+            }
+            else
+            {
+                botonEncendido.BackColor = Color.Red;
+                etiquetaValorFrecuencia.Text = "";
+                etiquetaValorVolumen.Text = "";
+
+                botonSubeVolumen.Enabled = false;
+                botonBajaVolumen.Enabled = false;
+
+                botonSubeFrecuencia.Enabled = false;
+                botonBajaFrecuencia.Enabled = false;
+            }
+        }
+
+        private void botonSubeVolumen_Click(object sender, EventArgs e)
+        {
+            SubirVolumen();
+            etiquetaValorVolumen.Text = volumen.ToString();
+        }
+
+        private void botonBajaVolumen_Click(object sender, EventArgs e)
+        {
+            BajarVolumen();
             etiquetaValorVolumen.Text = volumen.ToString();
 
-            botonSubeVolumen.Enabled = true;
-            botonBajaVolumen.Enabled = true;
-
-            botonSubeFrecuencia.Enabled = true;
-            botonBajaFrecuencia.Enabled = true;
         }
-        else
+
+        private void botonSubeFrecuencia_Click(object sender, EventArgs e)
         {
-            botonEncendido.BackColor = Color.Red;
-            etiquetaValorFrecuencia.Text = "";
-            etiquetaValorVolumen.Text = "";
-
-            botonSubeVolumen.Enabled = false;
-            botonBajaVolumen.Enabled = false;
-
-            botonSubeFrecuencia.Enabled = false;
-            botonBajaFrecuencia.Enabled = false;
+            SubirFrecuencia();
+            etiquetaValorFrecuencia.Text = frecuencia.ToString();
         }
-    }
 
-    private void botonSubeVolumen_Click(object sender, EventArgs e)
-    {
-        SubirVolumen();
-        etiquetaValorVolumen.Text = volumen.ToString();
-    }
-
-    private void botonBajaVolumen_Click(object sender, EventArgs e)
-    {
-        BajarVolumen();
-        etiquetaValorVolumen.Text = volumen.ToString();
-
-    }
-
-    private void botonSubeFrecuencia_Click(object sender, EventArgs e)
-    {
-        SubirFrecuencia();
-        etiquetaValorFrecuencia.Text = frecuencia.ToString();
-    }
-
-    private void botonBajaFrecuencia_Click(object sender, EventArgs e)
-    {
-        BajarFrecuencia();
-        etiquetaValorFrecuencia.Text = frecuencia.ToString();
-    }
+        private void botonBajaFrecuencia_Click(object sender, EventArgs e)
+        {
+            BajarFrecuencia();
+            etiquetaValorFrecuencia.Text = frecuencia.ToString();
+        }
 
         //Metodos
         public void SubirVolumen()
