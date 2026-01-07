b67bdc6a10de3a645aeae8e5e1d6df4d4d69dedb
Juan Dario Rodas M. <jdrodas@hotmail.com>
2026-01-07 17:39:14 -0500

    Actualizado proyecto TiendaBicicletasArchivosXML

--- TiendaBicicletasArchivosXML/TiendaBicicletasArchivosXML/Bicicleta.cs
+++ TiendaBicicletasArchivosXML/TiendaBicicletasArchivosXML/Bicicleta.cs
@@ -5,7 +5,7 @@ namespace TiendaBicicletasArchivosXML
     /// <summary>
     /// Representa una bicicleta en el inventario de la tienda
     /// </summary>
-    public class Bicicleta
+    public class Bicicleta : IEquatable<Bicicleta>
     {
         [XmlElement("Codigo")]
         public int Codigo { get; set; }
@@ -33,6 +33,60 @@ namespace TiendaBicicletasArchivosXML
                 $"\tCambios: {Cambios}\n" +
                 $"\tColor: {Color}\n" +
                 $"\tTracción: {Traccion}";
+        }
+
+        public bool Equals(Bicicleta? otraBicicleta)
+        {
+            if (otraBicicleta is null) return false;
+            if (ReferenceEquals(this, otraBicicleta)) return true;
+
+            return Codigo == otraBicicleta.Codigo &&
+                Marca == otraBicicleta.Marca &&
+                Tamaño == otraBicicleta.Tamaño &&
+                Cambios == otraBicicleta.Cambios &&
+                Color == otraBicicleta.Color &&
+                Traccion == otraBicicleta.Traccion;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Bicicleta);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(
+                Codigo,
+                Marca,
+                Tamaño,
+                Cambios,
+                Color,
+                Traccion);
+        }
+        public static bool operator ==(Bicicleta? izquierda, Bicicleta? derecha)
+        {
+            if (izquierda is null)
+                return derecha is null;
+
+            return izquierda.Equals(derecha);
+        }
+
+        public static bool operator !=(Bicicleta? izquierda, Bicicleta? derecha)
+        {
+            return !(izquierda == derecha);
+        }
+
+        public Bicicleta Clonar()
+        {
+            return new Bicicleta
+            {
+                Codigo = this.Codigo,
+                Marca = this.Marca,
+                Tamaño = this.Tamaño,
+                Cambios = this.Cambios,
+                Color = this.Color,
+                Traccion = this.Traccion
+            };
         }
     }
 }
